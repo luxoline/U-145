d62a93c30ab93d798245406e0b7e594b32224214
Denizhan Keleş <denizhank99@hotmail.com>
2023-07-10 17:33:51 +0300

    Npc Controller Updated

--- Assets/Scripts/NPC/NpcController.cs
+++ Assets/Scripts/NPC/NpcController.cs
@@ -17,25 +17,36 @@ public class NpcController : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();  
         animator = GetComponent<Animator>();    
-        PathPoints = new Transform[Path.transform.childCount];
-        for(int i =0; i<PathPoints.Length; i++) PathPoints[i] = Path.transform.GetChild(i);
+        //PathPoints = new Transform[Path.transform.childCount];
+        //for(int i =0; i<PathPoints.Length; i++) PathPoints[i] = Path.transform.GetChild(i);
     }
 
     void Update()
     {
         MoveNpc();
-        Debug.Log(index);
+        /*Debug.Log((int)transform.position.x == (int)PathPoints[PathPoints.Length - 1].transform.position.x);
+        Debug.Log("Kadýn: "+ transform.position.x);
+        Debug.Log("Nokta: "+ PathPoints[PathPoints.Length - 1].transform.position.x);*/
         Debug.Log(Vector3.Distance(transform.position, PathPoints[index].transform.position));
+
     }
 
     void MoveNpc()
     {
-        if (Vector3.Distance(transform.position, PathPoints[index].transform.position) < minDistance)
+        if (!agent.isStopped)
         {
-             if (index >= 0 && index < PathPoints.Length) index++; 
-             else index = 0;
-        } 
-        agent.SetDestination(PathPoints[index].position);
-        animator.SetFloat("vertical", 1);
+            if (Vector3.Distance(transform.position, PathPoints[index].transform.position) < minDistance)
+            {
+                if (index >= 0 && index < PathPoints.Length - 1) index = (index + 1) % PathPoints.Length; 
+                agent.SetDestination(PathPoints[index].position);
+            }
+            animator.SetFloat("vertical", 1);
+            if (transform.position.x.ToString("F2") ==PathPoints[PathPoints.Length - 1].transform.position.x.ToString("F2"))
+            {
+                agent.isStopped = true;
+            }
+        }
+       else animator.SetFloat("vertical", 0);
+
     }
 }
